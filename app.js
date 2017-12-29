a9acde2e81b792616d5c5523eb1493eaa3285ac0
Patrick Brock <patrick.brock@evverbank.com>
2017-12-29 09:06:11 -0500

    removes some unneded reqs
    Removes libraries left over from mongo

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var bluebird = require('bluebird')
 
 var app = express();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
 var api = require('./routes/api.route')
 
 app.use(function(req, res, next) {
@@ -24,12 +19,9 @@ app.use(function(req, res, next) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
